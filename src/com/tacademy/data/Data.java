4c3c0a98d25189f5410a92c0291e9a648f988098
xellos <xelloss007@gmail.com>
2017-07-21 09:26:48 +0900

    name add

--- src/com/tacademy/data/Data.java
+++ src/com/tacademy/data/Data.java
@@ -2,8 +2,24 @@ package com.tacademy.data;
 
 public class Data {
 
+	private String name;
 	public Data() {
 		// TODO Auto-generated constructor stub
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Data [name=" + name + "]";
 	}
 
 	public static void main(String[] args) {
