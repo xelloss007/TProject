ff75c11e875865ea467db26082d1a3cac1314253
xellos <xelloss007@gmail.com>
2017-07-21 09:21:03 +0900

    first commit

---
+++ src/com/tacademy/data/Data.java
@@ -0,0 +1,14 @@
+package com.tacademy.data;
+
+public class Data {
+
+	public Data() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Data();
+	}
+
+}
